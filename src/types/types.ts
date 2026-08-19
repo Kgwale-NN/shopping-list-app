4257fef30ec85e20a111dcbd2aebc7f17e5c0b1f
Kgwale NN <kgoalemothiba8@gmail.com>
2026-08-19 07:56:31 +0200

    Add TypeScript type definitions

---
+++ src/types/types.ts
@@ -0,0 +1,39 @@
+export interface User {
+
+    id: string,
+    email: string,
+    name: string,
+    surname: string,
+    cellNumber: string,
+    password?: string
+
+}
+
+export interface LoginCredentials {
+
+    email: string,
+    password: string
+
+}
+
+export interface RegisterData{
+
+email : string,
+password: string,
+name: string,
+surname: string,
+cellNumber: string
+
+}
+
+export interface ShoppingListItem {
+
+    id: string,
+    name: string,
+    quantity: number,
+    notes?: string,
+    category: string,
+    image?: string,
+    useId: string,
+    dateAdded: string
+}
