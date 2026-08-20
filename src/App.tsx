c8b6dec78c434482c9b23414a829757a429b28e5
Kgwale NN <kgoalemothiba8@gmail.com>
2026-08-20 11:35:47 +0200

    Add React Router navigation between Login and Register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css'
-import LoginPage from './components/auth/LoginPage'
+import { Routes, Route } from 'react-router-dom'
+import { LoginPage } from './components/auth/LoginPage'
 import { RegisterPage } from './components/auth/RegisterPage'
 
 function App() {
 
   return (
 
-    <>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
 
-        <RegisterPage />
-        <LoginPage />
-
-    </>
   )
 }
 
--- src/components/auth/RegisterPage.tsx
+++ src/components/auth/RegisterPage.tsx
@@ -129,5 +129,3 @@ export const RegisterPage: React.FC = () => {
 
   )
 }
-
-export default RegisterPage
